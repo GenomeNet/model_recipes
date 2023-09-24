1d442ba4100f3b2411786139f3d3d7356f877a74
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2023-09-24 23:18:44 +0200

    add check if file is generated

--- virusnet-gpu/conda.recipe/run_test.sh
+++ virusnet-gpu/conda.recipe/run_test.sh
@@ -3,9 +3,9 @@
 virusnet -i ${PREFIX}/tests/test.fasta -o ${PREFIX}/tests/output.csv
 
 # Check if the output file is created
-#if [ -f "${PREFIX}/tests/output.csv" ]; then
-#    echo "Output file created successfully."
-#else
-#    echo "Output file not found."
-#    exit 1
-#fi
+if [ -f "${PREFIX}/tests/output.csv" ]; then
+    echo "Output file created successfully."
+else
+    echo "Output file not found."
+    exit 1
+fi
