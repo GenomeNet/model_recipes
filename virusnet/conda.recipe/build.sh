c9d80681deadc2286601759a434cf6159de763f5
Philipp Muench <philipp.muench@helmholtz-hzi.de>
2023-10-11 00:08:22 +0200

    move deepG installation to build

--- virusnet/conda.recipe/build.sh
+++ virusnet/conda.recipe/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Download necessary files
 wget -P $PREFIX/lib/virusnet https://f000.backblazeb2.com/file/genomenet/models/virus_genus_2023-01-23.hdf5
 wget -P $PREFIX/lib/virusnet https://f000.backblazeb2.com/file/genomenet/models/virus_genus_2023-01-23_labels.rds
 
@@ -13,3 +14,7 @@ cp $SRC_DIR/predict.r $PREFIX/bin/predict.r  # copy it to the bin directory
 
 # Make the script executable
 chmod +x $PREFIX/bin/virusnet
+
+# Install the R package
+echo "Installing the R package from GitHub..."
+R -e "devtools::install_github('genomenet/deepg')"
--- virusnet/conda.recipe/post-link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# install deepg package
-"$PREFIX/bin/R" -e "remotes::install_github('GenomeNet/deepg')"
